65bb3cc8cca3656e9c0763d449d73a0ccceadd13
slavamuravey <slavamuravey@mail.ru>
2024-07-15 16:38:18 +0700

    Simplify uv__update_time

--- src/internal.h
+++ src/internal.h
@@ -7,21 +7,26 @@
 #include <inttypes.h>
 #include <time.h>
 
+#if defined(__clang__) ||                                                     \
+    defined(__GNUC__) ||                                                      \
+    defined(__INTEL_COMPILER)
+# define UV_UNUSED(declaration)     __attribute__((unused)) declaration
+#else
+# define UV_UNUSED(declaration)     declaration
+#endif
+
 void uv__io_poll(uv_loop_t* loop, int timeout);
 
 int uv__platform_loop_init(uv_loop_t* loop);
 
-static void uv__update_time(uv_loop_t* loop) {
+UV_UNUSED(static void uv__update_time(uv_loop_t* loop)) {
   struct timespec t;
-  clock_t clock_id;
 
-  clock_id = CLOCK_MONOTONIC;
-
-  if (clock_gettime(clock_id, &t)) {
+  if (clock_gettime(CLOCK_MONOTONIC, &t)) {
     loop->time = 0;
   }
 
-  loop->time = (t.tv_sec * (uint64_t) 1e9 + t.tv_nsec) / 1000000;
+  loop->time = (t.tv_sec * (uint64_t) 1e9 + t.tv_nsec) / 1e6;
 }
 
 #endif

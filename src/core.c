321cdad1ec868853c5e8eea6d3e227ee544c25f7
slavamuravey <slavamuravey@mail.ru>
2024-07-16 17:35:51 +0700

    Fix timeout calculation

--- src/core.c
+++ src/core.c
@@ -5,25 +5,32 @@ static int uv__loop_alive(const uv_loop_t* loop) {
   return loop->active_handles > 0;
 }
 
+static int uv__backend_timeout(const uv_loop_t* loop) {
+  if (loop->active_handles > 0) {
+    return uv__next_timeout(loop);
+  }
+  return 0;
+}
+
 int uv_run(uv_loop_t* loop, uv_run_mode mode) {
   int timeout;
-  int has_active_handles;
+  int r;
 
-  has_active_handles = uv__loop_alive(loop);
+  r = uv__loop_alive(loop);
   uv__update_time(loop);
 
-  if (has_active_handles) {
+  if (r) {
     uv__run_timers(loop);
   }
 
-  while (has_active_handles) {
-    timeout = uv__next_timeout(loop);
+  while (r) {
+    timeout = uv__backend_timeout(loop);
     uv__io_poll(loop, timeout);
 
     uv__update_time(loop);
     uv__run_timers(loop);
 
-    has_active_handles = uv__loop_alive(loop);
+    r = uv__loop_alive(loop);
   }
 
   return 0;

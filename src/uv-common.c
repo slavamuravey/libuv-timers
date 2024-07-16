adc5e05bdcacc18c8e46f8add720de3167a3faab
slavamuravey <slavamuravey@mail.ru>
2024-07-16 13:09:49 +0700

    Fix uv_unref function

--- src/uv-common.c
+++ src/uv-common.c
@@ -1,5 +1,14 @@
 #include "uv.h"
+#include "uv-common.h"
 
 void uv_unref(uv_handle_t* handle) {
-  handle->loop->active_handles--;       
+  if ((handle->flags & UV_HANDLE_REF) == 0) {
+    return;
+  }                       
+  
+  handle->flags &= ~UV_HANDLE_REF;                                             
+  
+  if (handle->flags & UV_HANDLE_ACTIVE) {
+    handle->loop->active_handles--;
+  }
 }

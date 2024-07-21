a5cd12a585d567a67bacf3940682bce66fa7588d
slavamuravey <slavamuravey@mail.ru>
2024-07-21 19:22:35 +0700

    Add example of timers usage

--- examples/timers/main.c
+++ examples/timers/main.c
@@ -2,39 +2,34 @@
 
 #include <uv.h>
 
-uv_timer_t timer3;
+struct timer {
+  uv_timer_t timer;
+  uint64_t timeout;
+};
 
-void timer3_callback(uv_timer_t *handle);
-
-void timer1_callback(uv_timer_t *handle) {
-    fprintf(stdout, "timer1 callback invoked\n");
-}
-
-void timer2_callback(uv_timer_t *handle) {
-    uv_timer_init(handle->loop, &timer3);
-    uv_timer_start(&timer3, timer3_callback, 1000, 0);
-    fprintf(stdout, "timer2 callback invoked\n");
-}
-
-void timer3_callback(uv_timer_t *handle) {
-    fprintf(stdout, "timer3 callback invoked\n");
+void timer_callback(uv_timer_t *handle) {
+    printf("timer callback invoked, timeout = %ld\n", handle->timeout);
 }
 
 int main() {
-    uv_loop_t loop;
-    uv_timer_t timer1;
-    uv_timer_t timer2;
+  int i;
+  uv_loop_t loop;
+  struct timer timers[] = {
+    { timeout: 6000 }, 
+    { timeout: 3000 },
+    { timeout: 2000 },
+    { timeout: 1000 },
+    { timeout: 4000 },
+    { timeout: 5000 },
+  };
 
-    uv_loop_init(&loop);
+  uv_loop_init(&loop);
 
-    uv_timer_init(&loop, &timer1);
-    uv_unref((uv_handle_t*) &timer1);
-
-    uv_timer_start(&timer1, timer1_callback, 0, 2000);
-    uv_timer_stop(&timer1);
-
-    uv_timer_init(&loop, &timer2);
-    uv_timer_start(&timer2, timer2_callback, 9000, 0);
-    
-    return uv_run(&loop, UV_RUN_DEFAULT);
+  for (i = 0; i < sizeof(timers) / sizeof(*timers); i++) {
+    struct timer *timer = &timers[i];
+    uv_timer_init(&loop, &timer->timer);
+    uv_timer_start(&timer->timer, timer_callback, timer->timeout, 0);
+  }
+  
+  return uv_run(&loop, UV_RUN_DEFAULT);
 }
